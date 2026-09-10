96fee087bcbaf6281b703d6ba82a6e2a0780e2b7
mkc7781-boop <mkc7781-boop@users.noreply.github.com>
2026-09-10 13:24:59 -0700

    Fix html brightness hydration mismatch.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta name="theme-color" content="#0f766e" />
